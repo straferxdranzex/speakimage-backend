97c1759f66bfbe97f550428b04374ebe5dbbeddf
Omer Aayan <umerayan6@gmail.com>
2024-05-21 23:15:57 +0500

    Update CORS

--- app.py
+++ app.py
@@ -17,7 +17,11 @@ logging.basicConfig(level=logging.DEBUG)  # Set logging level to debug for detai
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://speakimage.ai"}})
+CORS(app, supports_credentials=True, resources={
+    r"/*": {
+        "origins": ["https://speakimage.ai", "http://localhost:3000"]
+    }
+})
 
 app.secret_key = os.getenv("SECRET_KEY")
 app.permanent_session_lifetime = timedelta(days=15)
