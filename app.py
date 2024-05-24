9c5b12371d5b5730d1427fb93b70059a0fe37c9d
Omer Aayan <umerayan6@gmail.com>
2024-05-24 10:47:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.DEBUG)  # Set logging level to debug for detai
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 app.secret_key = os.getenv("SECRET_KEY")
 app.permanent_session_lifetime = timedelta(days=15)
